e7355bdfd518192b658d74da99d813bc7d162bee
Yerim <dpfla725@naver.com>
2022-10-15 19:35:27 +0900

    Initial Commiit Test

---
+++ src/main/java/regiPaymentMethod/controller/RegiPaymentController.java
@@ -0,0 +1,20 @@
+package regiPaymentMethod.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HomeTT/mypage")
+public class RegiPaymentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  @Override
+protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	  System.out.println("Hello Test Code ");
+  
+  }
+}
