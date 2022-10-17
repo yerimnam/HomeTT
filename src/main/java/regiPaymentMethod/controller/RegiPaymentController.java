e50cf45cd1e5c0dc3af706b07f42ed08c6aa744f
Yerim <dpfla725@naver.com>
2022-10-17 14:16:21 +0900

    추가222

--- src/main/java/regiPaymentMethod/controller/RegiPaymentController.java
+++ src/main/java/regiPaymentMethod/controller/RegiPaymentController.java
@@ -16,6 +16,7 @@ protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws Se
 
 	  System.out.println("Hello Test Code ");
 	  System.out.println("Hello Test Code ");//추가
+	  System.out.println("Hello Test Code2222 ");//추가
 	  
   
   }
