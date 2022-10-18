623bfb724cb1261a31b99fccf1d10cf0f5931ff4
My Com <My Com@DESKTOP-SFB7JR7>
2022-10-18 18:41:58 +0900

    wook - 18:41

--- src/main/java/partyExit/controller/PartyExitContorller.java
+++ src/main/java/partyExit/controller/PartyExitContorller.java
@@ -15,5 +15,6 @@ public class PartyExitContorller extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("/homett/exit [GET]");
+		
 	}
 }
