411fa3700c7e2248b4af8fa65ab834b9dae355ce
My Com <My Com@DESKTOP-SFB7JR7>
2022-10-18 18:00:49 +0900

    commit wook- 18:00

--- src/main/java/partyCheck/service/impl/CreatePartyCheckServiceImpl.java
+++ src/main/java/partyCheck/service/impl/CreatePartyCheckServiceImpl.java
@@ -1,7 +1,24 @@
 package partyCheck.service.impl;
 
+import java.util.List;
+
+import common.JDBCTemplate;
+import partyCheck.dao.face.CreatePartyCheckDao;
+import partyCheck.dao.impl.CreatePartyCheckDaoImpl;
+import partyCheck.dto.PartyCheck;
 import partyCheck.service.face.CreatePartyCheckService;
 
 public class CreatePartyCheckServiceImpl implements CreatePartyCheckService {
+
+	// DAO 객체 생성
+	private CreatePartyCheckDao createPartyCheckDao = new CreatePartyCheckDaoImpl();
+	
+	@Override
+	public List<PartyCheck> getList() {
+		System.out.println("CreatePartyService getList()");
+
+		
+		return createPartyCheckDao.selectAll(JDBCTemplate.getConnection());
+	}
 
 }
