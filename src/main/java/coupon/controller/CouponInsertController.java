62531eaae76421dd4ea3ad6e9b0e184f922c9ce6
Yerim <dpfla725@naver.com>
2022-10-18 18:21:14 +0900

    change and add servlet

---
+++ src/main/java/coupon/controller/CouponInsertController.java
@@ -0,0 +1,13 @@
+package coupon.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class CouponInsertController
+ */
+@WebServlet("/homtt/couponinsert")
+public class CouponInsertController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/point/controller/PointChargeController.java
@@ -0,0 +1,13 @@
+package point.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class PointChargeController
+ */
+@WebServlet("/homett/poincharge")
+public class PointChargeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
