160707eefa2464f412cfe7317883f9fac74cb022
My Com <My Com@DESKTOP-SFB7JR7>
2022-10-18 17:45:13 +0900

    partycheck modify - wook 17:44

--- src/main/java/partyCheck/dto/PartyCheck.java
+++ src/main/java/partyCheck/dto/PartyCheck.java
@@ -1,5 +1,15 @@
 package partyCheck.dto;
 
+import java.util.Date;
+
 public class PartyCheck {
 
+	private int partyno;
+	private int userno;
+	private int partycono;
+	private String partykind;
+	private String parryrule;
+	private int paymentamount;
+	private Date partyperiod;
+	
 }
