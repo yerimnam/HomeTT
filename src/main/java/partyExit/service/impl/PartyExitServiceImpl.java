125f405a1b64003d4b84edf06332b0ae0be6ef6e
My Com <My Com@DESKTOP-SFB7JR7>
2022-10-24 22:43:17 +0900

    10/24 partyExit invitation modify - wook 22:48

--- src/main/java/partyExit/service/impl/PartyExitServiceImpl.java
+++ src/main/java/partyExit/service/impl/PartyExitServiceImpl.java
@@ -5,21 +5,42 @@ import java.sql.Connection;
 import javax.servlet.http.HttpServletRequest;
 
 import common.JDBCTemplate;
+import partyExit.dao.face.PartyExitDao;
+import partyExit.dao.impl.PartyExitDaoImpl;
 import partyExit.dto.PartyExit;
 import partyExit.service.face.PartyExitService;
 
 public class PartyExitServiceImpl implements PartyExitService {
 
+	// DAO 객체
+	private PartyExitDao partyExitDao = new PartyExitDaoImpl();
+	
 	@Override
 	public PartyExit getPartyNo(HttpServletRequest req) {
-		// TODO Auto-generated method stub
-		return null;
+
+		// 전달파라미터를 저장할 객체 생성
+		PartyExit partyExit = new PartyExit();
+		
+		// 전달파라미터 partyNo 추출
+		String param = req.getParameter("partyNo");
+		if(null != param && !"".equals(param)) {
+			partyExit.setPartyNo(Integer.parseInt(param));
+		}
+		
+		return partyExit;
 	} 
 
 	@Override
-	public void delete(PartyExit partyexit) {
+	public void delete(PartyExit partyExit) {
 
 		Connection conn = JDBCTemplate.getConnection();
+		
+		// 파티방 나가기
+		if(partyExitDao.delete(conn, partyExit) > 0) {
+			JDBCTemplate.commit(conn);
+		} else {
+			JDBCTemplate.rollback(conn);
+		}
 		
 	}
 
