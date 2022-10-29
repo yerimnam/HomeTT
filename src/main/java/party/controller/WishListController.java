4fe78a719f33269127726f9913c40a0c5ed08a8e
jiwon <jiwon@DESKTOP-AR4UU3O>
2022-10-29 18:43:25 +0900

    221029 - 지원 - 백업한거 커밋

--- src/main/java/party/controller/WishListController.java
+++ src/main/java/party/controller/WishListController.java
@@ -1,6 +1,8 @@
 package party.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,13 +10,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import party.dto.Party;
+import party.dto.WishList;
 import party.service.face.PartyModifyService;
 import party.service.face.WishListService;
 import party.service.impl.PartyModifyServiceImpl;
 import party.service.impl.WishListServiceImpl;
-import util.PrPaging;
 
 @WebServlet("/homett/wishlist")
 public class WishListController extends HttpServlet {
@@ -34,8 +36,14 @@ public class WishListController extends HttpServlet {
 			
 			System.out.println("/homett/wishlist [GET]");
 			
+			HttpSession session = req.getSession();
+//			session.setAttribute("user_no", 1); // 테스트용 유저 정보 받아오기
+//			int userNo = Integer.parseInt(String.valueOf(session.getAttribute("userNo")));
+//			int userNo = (int) session.getAttribute("user_no");
+			
+			
 			//jsp 뷰 먼저 지정
-			req.getRequestDispatcher("/WEB-INF/party/wishList.jsp").forward(req, resp);
+			req.getRequestDispatcher("/WEB-INF/party/partyModify.jsp").forward(req, resp);
 		
 			
 		}
@@ -46,11 +54,29 @@ public class WishListController extends HttpServlet {
 		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			
 			System.out.println("/homett/wishlist [POST]");
+
+//			resp.setContentType("application/json;charset=utf-8");
+			
+			HttpSession session = req.getSession();
+			session.setAttribute("userNo", 1); // 테스트용 유저 정보 받아오기
+//			int userNo = Integer.parseInt(String.valueOf(session.getAttribute("userNo")));
+			
+//			String partyNo = req.getParameter("party_no"); //파티 정보 받아오기
+			
+			//추후 세션값으로 대치
+			int userNo = 1;
+			String partyNo = req.getParameter("partyNo"); //파티 정보 받아오기
+			
+			System.out.println("partyNo :"+ partyNo);
 			
 			//글 삽입하기
 			wishlistService.wishinsert(req);
 			
+//			PrintWriter out = resp.getWriter();
 			
+			
+			req.getRequestDispatcher("/WEB-INF/party/wishList.jsp").forward(req, resp);
+//			doGet(req, resp);
 			
 		}
 
